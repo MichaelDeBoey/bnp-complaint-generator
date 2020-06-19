653be861f89a68a9ba4892ca3950bc2b289d14a0
Michaël De Boey <info@michaeldeboey.be>
2020-06-19 19:42:34 +0200

    Extract Form

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,6 +1,6 @@
 import React, { FunctionComponent } from 'react';
 
-import { FormInput } from './FormInput';
+import { Form } from './Form';
 import { Intro } from './Intro';
 import { Pattern } from './Pattern';
 
@@ -14,29 +14,7 @@ export const App: FunctionComponent = () => {
 
         <Intro />
 
-        <div className="mt-12 grid grid-cols-1 row-gap-6 sm:grid-cols-2 sm:col-gap-8">
-          <FormInput id="first_name" label="First name" />
-
-          <FormInput id="last_name" label="Last name" />
-
-          <FormInput
-            className="sm:col-span-2"
-            id="client_id"
-            label="Client ID (available on your bank card)"
-          />
-
-          <div className="sm:col-span-2">
-            <span className="w-full inline-flex rounded-md shadow-sm">
-              <button
-                type="button"
-                disabled
-                className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-500 focus:outline-none focus:border-indigo-700 focus:shadow-outline-indigo active:bg-indigo-700 transition ease-in-out duration-150"
-              >
-                ⚙️ Generate template
-              </button>
-            </span>
-          </div>
-        </div>
+        <Form />
       </div>
     </div>
   );
