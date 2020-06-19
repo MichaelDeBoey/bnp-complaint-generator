4e27d52a3c0aa103644f6e0702e2be497594a36b
Michaël De Boey <info@michaeldeboey.be>
2020-06-19 18:59:40 +0200

    Extract Pattern

---
+++ src/App/Pattern.tsx
@@ -0,0 +1,37 @@
+import React, { FunctionComponent, SVGProps } from 'react';
+
+type Props = Pick<SVGProps<SVGSVGElement>, 'className'>;
+export const Pattern: FunctionComponent<Props> = ({ className }) => (
+  <svg
+    className={className}
+    width="404"
+    height="404"
+    fill="none"
+    viewBox="0 0 404 404"
+  >
+    <defs>
+      <pattern
+        id="85737c0e-0916-41d7-917f-596dc7edfa27"
+        x="0"
+        y="0"
+        width="20"
+        height="20"
+        patternUnits="userSpaceOnUse"
+      >
+        <rect
+          x="0"
+          y="0"
+          width="4"
+          height="4"
+          className="text-gray-200"
+          fill="currentColor"
+        />
+      </pattern>
+    </defs>
+    <rect
+      width="404"
+      height="404"
+      fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
+    />
+  </svg>
+);
--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,75 +1,14 @@
 import React, { FunctionComponent } from 'react';
 
+import {Pattern} from "./Pattern";
 
 export const App: FunctionComponent = () => {
   return (
     <div className="bg-white py-16 px-4 overflow-hidden sm:px-6 lg:px-8 lg:py-24">
       <div className="relative max-w-xl mx-auto">
-        <svg
-          className="absolute left-full transform translate-x-1/2"
-          width="404"
-          height="404"
-          fill="none"
-          viewBox="0 0 404 404"
-        >
-          <defs>
-            <pattern
-              id="85737c0e-0916-41d7-917f-596dc7edfa27"
-              x="0"
-              y="0"
-              width="20"
-              height="20"
-              patternUnits="userSpaceOnUse"
-            >
-              <rect
-                x="0"
-                y="0"
-                width="4"
-                height="4"
-                className="text-gray-200"
-                fill="currentColor"
-              />
-            </pattern>
-          </defs>
-          <rect
-            width="404"
-            height="404"
-            fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
-          />
-        </svg>
+        <Pattern className="absolute left-full transform translate-x-1/2" />
 
-        <svg
-          className="absolute right-full bottom-0 transform -translate-x-1/2"
-          width="404"
-          height="404"
-          fill="none"
-          viewBox="0 0 404 404"
-        >
-          <defs>
-            <pattern
-              id="85737c0e-0916-41d7-917f-596dc7edfa27"
-              x="0"
-              y="0"
-              width="20"
-              height="20"
-              patternUnits="userSpaceOnUse"
-            >
-              <rect
-                x="0"
-                y="0"
-                width="4"
-                height="4"
-                className="text-gray-200"
-                fill="currentColor"
-              />
-            </pattern>
-          </defs>
-          <rect
-            width="404"
-            height="404"
-            fill="url(#85737c0e-0916-41d7-917f-596dc7edfa27)"
-          />
-        </svg>
+        <Pattern className="absolute right-full bottom-0 transform -translate-x-1/2" />
 
         <div className="text-center">
           <h2 className="text-3xl leading-9 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
@@ -171,6 +110,96 @@ export const App: FunctionComponent = () => {
             </span>
           </div>
         </div>
+
+        {/*<div>*/}
+        {/*  <p className="mt-4 text-lg leading-6 text-gray-500">*/}
+        {/*    Hierbij wil ik een klacht neerleggen tegen BNP Paribas Fortis en*/}
+        {/*    deze koppelen aan de klacht met nummer KQ10440226.*/}
+        {/*  </p>*/}
+
+        {/*  <p className="mt-4 text-lg leading-6 text-gray-500">*/}
+        {/*    Ik wens namelijk mijn zichtrekening bij deze bank te koppelen aan*/}
+        {/*    Cake, een betalingsinstelling die een vergunning heeft bij de*/}
+        {/*    Nationale Bank van België.*/}
+        {/*  </p>*/}
+
+        {/*  <p className="mt-4 text-lg leading-6 text-gray-500">*/}
+        {/*    Sinds 14 september 2019 is mijn bank door de Europese*/}
+        {/*    PSD2-regelgeving verplicht om een interface aan te bieden aan*/}
+        {/*    partijen zoals Cake zodat ik mijn bankgegevens kan delen met Cake.*/}
+        {/*  </p>*/}
+
+        {/*  <p className="mt-4 text-lg leading-6 text-gray-500">*/}
+        {/*    BNP Paribas Fortis heeft tot op vandaag nog steeds geen werkende*/}
+        {/*    interface waardoor ik geen gebruik kan maken van de diensten van*/}
+        {/*    Cake. Deze diensten zorgen ervoor dat hun gebruikers geld kunnen*/}
+        {/*    verdienen via cashbacks of door het delen van hun data met Cake. Ik*/}
+        {/*    las dat de gemiddelde gebruiker op een paar maanden tijd reeds 10*/}
+        {/*    euro kon verdienen met Cake.*/}
+        {/*  </p>*/}
+
+        {/*  <p className="mt-4 text-lg leading-6 text-gray-500">*/}
+        {/*    Zolang BNP Paribas Fortis geen werkende interface heeft, mis ik de*/}
+        {/*    gelden waarop iedere Cake-gebruiker recht heeft. De gratis diensten*/}
+        {/*    van Cake worden aangeboden aan alle klanten met een Belgische*/}
+        {/*    bankrekening, enkel de klanten van BNP Paribas Fortis kunnen er niet*/}
+        {/*    van genieten omdat deze bank geen werkende interface heeft. En*/}
+        {/*    volgens de wetgeving is dit verplicht sinds 14 september 2019.*/}
+        {/*  </p>*/}
+
+        {/*  <p className="mt-4 text-lg leading-6 text-gray-500">*/}
+        {/*    Op social media heb ik gelezen dat BNP Paribas Fortis aan Cake het*/}
+        {/*    volgende heeft laten weten:*/}
+        {/*  </p>*/}
+
+        {/*  <p className="mt-4 text-lg leading-6 text-gray-500">*/}
+        {/*    "There is indeed a difference between the APIs and our own channels,*/}
+        {/*    and it cannot be solved on the short term. When there is a fix*/}
+        {/*    available you will of course be invited to test it but I am afraid I*/}
+        {/*    cannot give you a timeline yet at this stage."*/}
+        {/*  </p>*/}
+
+        {/*  <p className="mt-4 text-lg leading-6 text-gray-500">*/}
+        {/*    Dat betekent dat BNP Paribas Fortis geen oplossing heeft op korte*/}
+        {/*    termijn terwijl we vandaag 9 maanden voorbij de wettelijke deadline*/}
+        {/*    zijn.*/}
+        {/*  </p>*/}
+
+        {/*  <p className="mt-4 text-lg leading-6 text-gray-500">*/}
+        {/*    Daarom wil ik van BNP Paribas Fortis een schadevergoeding ontvangen*/}
+        {/*    die minstens gelijk is aan het bedrag dat ik van Cake zou ontvangen*/}
+        {/*    moest de bank voldoen aan de regelgeving en dit zolang de bank geen*/}
+        {/*    werkende interface heeft.*/}
+        {/*  </p>*/}
+
+        {/*  <p className="mt-4 text-lg leading-6 text-gray-500">*/}
+        {/*    Met vriendelijke groeten,*/}
+        {/*  </p>*/}
+
+        {/*  <p className="mt-4 text-lg leading-6 text-gray-500">*/}
+        {/*    Michaël De Boey<br />*/}
+        {/*    11472 82618*/}
+        {/*  </p>*/}
+        {/*</div>*/}
+
+        {/*<div className="mt-12 grid grid-cols-1 row-gap-6 sm:grid-cols-2 sm:col-gap-8">*/}
+        {/*  <div className="sm:col-span-2">*/}
+        {/*    <label*/}
+        {/*      htmlFor="message"*/}
+        {/*      className="block text-sm font-medium leading-5 text-gray-700"*/}
+        {/*    >*/}
+        {/*      Message*/}
+        {/*    </label>*/}
+        {/*    <div className="mt-1 relative rounded-md shadow-sm">*/}
+        {/*      <textarea*/}
+        {/*        id="message"*/}
+        {/*        rows={4}*/}
+        {/*        readOnly*/}
+        {/*        className="form-textarea py-3 px-4 block w-full transition ease-in-out duration-150"*/}
+        {/*      />*/}
+        {/*    </div>*/}
+        {/*  </div>*/}
+        {/*</div>*/}
       </div>
     </div>
   );
