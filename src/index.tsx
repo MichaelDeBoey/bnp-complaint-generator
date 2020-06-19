7379aea93f23c9418fa7d91b59a921fb2b5962c4
Michaël De Boey <info@michaeldeboey.be>
2020-06-19 18:57:40 +0200

    Add tailwindcss

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import { render } from 'react-dom';
 
 import { App } from './App';
-import './index.css';
+import './index.generated.css';
 
 render(
   <StrictMode>
